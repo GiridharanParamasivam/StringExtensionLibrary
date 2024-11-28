5147324c0431d98a22b54cd4627d277342edad70
Giridharan Paramasivam <gparamas@my.centennialcollege.ca>
2024-11-27 20:54:58 -0500

    initial commit

---
+++ StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensionLibrary
+{
+    internal static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+            char firstChar = input[0];
+            return char.IsUpper(firstChar);
+        }
+    }
+}
